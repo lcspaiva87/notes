8105bae82240d06cec0c9921b02f88d0aa32c8f3
lucas paiva <lcspaiva87@gmail.com>
2024-02-05 23:26:06 -0300

    Atualizações de layout e tradução para pt-br

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,38 @@ export default function Home() {
         </form>
 
         <div className="h-px w-full bg-slate-700 " />
-        <div className="grid grid-cols-3 auto-rows-[250px]">
-          <Content />
-
+        <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+          <Content bg="bg-slate-700" />
+          <div className="overflow-hidden  px-5 bg-slate-800 p-5 rounded-md relative">
+            <span className="text-slate-300 font-medium text-sm">
+              Adicionar nota
+            </span>
+            <p className="text-slate-400 text-sm leading-6 ">
+              Grave uma nota em áudio que será convertida para texto
+              automaticamente.
+            </p>
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+          </div>
+          <div className="overflow-hidden  px-5 bg-slate-800 p-5 rounded-md relative">
+            <span className="text-slate-300 font-medium text-sm">
+              Adicionar nota
+            </span>
+            <p className="text-slate-400 text-sm leading-6 ">
+              Grave uma nota em áudio que será convertida para texto
+              automaticamente.
+            </p>
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+          </div>
+          <div className="overflow-hidden  px-5 bg-slate-800 p-5 rounded-md relative">
+            <span className="text-slate-300 font-medium text-sm">
+              Adicionar nota
+            </span>
+            <p className="text-slate-400 text-sm leading-6 ">
+              Grave uma nota em áudio que será convertida para texto
+              automaticamente.
+            </p>
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+          </div>
         </div>
       </div>
     </Container>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,21 +1,20 @@
 import { ArrowUpRight } from "lucide-react";
 import { Button } from "./ui/button";
-import { Label } from "./ui/label";
 
-export function Content() {
+export function Content({bg}: {bg: string}) {
   return (
-    <div className="w-[21.75rem] h-[15.625rem]  px-5 bg-slate-700 p-[1.25rem] rounded-sm ">
-      <Button className="flex relative left-[18rem] -top-[1.3rem]">
+    <div className={`w-[21.75rem] h-[15.625rem]  px-5 ${bg} p-5 rounded-md `}>
+      <Button className="flex relative left-[17.6rem] -top-[1.3rem]">
         <ArrowUpRight size={16} />
       </Button>
       <div className="flex flex-col gap-[0.75rem]">
-        <Label className="text-slate-200 font-normal text-sm">
+        <span className="text-slate-200 font-medium text-sm">
           Adicionar nota
-        </Label>
-        <span className="text-slate-400 text-sm">
+        </span>
+        <p className="text-slate-400 text-sm leading-6 ">
           Grave uma nota em áudio que será convertida para texto
           automaticamente.
-        </span>
+        </p>
       </div>
     </div>
   );
