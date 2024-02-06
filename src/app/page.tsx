e1bd05d3e0a537c1b6c92d203bf0822cf41895ab
lucas paiva <lcspaiva87@gmail.com>
2024-02-05 23:09:30 -0300

    Home page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 import { Container } from "@/components/Container";
 import { Content } from "@/components/Content";
-import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <Container>
       <div className="flex flex-col gap-3  items-start">
-        <img src="/logo.svg" alt="" />
-        <Input
-          className=" border-none bg-transparent text-4xl font-bold w-full focus:border-none focus:outline-none"
-          placeholder="Busque em suas notas"
-        />
-        <Separator className="text-gray-300/30" />
+        <img src="/logo.svg" alt="Logo" />
+        <form action="" className="">
+          <input
+            type="text"
+            className=" w-full border-none bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500 text-slate-300"
+            placeholder="Busque em suas notas"
+          />
+        </form>
 
-        <Content/>
+        <div className="h-px w-full bg-slate-700 " />
+        <div className="grid grid-cols-3 auto-rows-[250px]">
+          <Content />
+
+        </div>
       </div>
     </Container>
   );
